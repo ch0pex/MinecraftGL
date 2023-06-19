23119fb2e7f43e2ab517a83e56d7e504ed88a742
Álvaru <78793330+ch0pex@users.noreply.github.com>
2023-06-19 14:59:27 +0000

    clear

--- src/common/public/GLDebug.h
+++
@@ -1,8 +0,0 @@
-#pragma once 
-
-#include <GL/glew.h>
-#include <iostream>
-
-GLenum checkError_(const char* file, int line); 
-
-#define glCheckError() checkError_(__FILE__, __LINE__) 
--- src/loaders/TextureLoader.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "../common/public/CommonHeaders.h"
-#include <GL/glew.h>
-
-class TextureLoader {
-public:
-	static u32 loadTexture(std::string fileName); 
-};
